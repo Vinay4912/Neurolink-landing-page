6ee35332567e5e9b68ba1a8627ea1255783e40fc
Vinay4912 <pawarvinay1580@gmail.com>
2025-05-04 20:08:04 +0530

    completed the ui

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from './ui/card'
+import { motion } from 'framer-motion';
 
 const Pricing = () => {
   return (
@@ -12,21 +12,27 @@ const Pricing = () => {
         </p>
       </div>
       <div className="mt-12 flex flex-wrap justify-center gap-6">
-        <div className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
+        <motion.div
+        whileHover={{ scale: 1.1}}
+        className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
           <p className='font-extrabold text-black text-lg sm:text-xl'>Basic Plan</p>
           <p className='text-gray-700 text-sm sm:text-base'>Access to essential features</p>
           <p className='mt-6 text-2xl font-bold'>$9.99/month</p>
-        </div>
-        <div className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
+        </motion.div>
+        <motion.div
+        whileHover={{ scale: 1.1}}
+         className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
           <p className='font-extrabold text-black text-lg sm:text-xl'>Pro Plan</p>
           <p className='text-gray-700 text-sm sm:text-base'>Unlock advanced features</p>
           <p className='mt-6 text-2xl font-bold'>$19.99/month</p>
-        </div>
-        <div className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
+        </motion.div>
+        <motion.div
+        whileHover={{ scale: 1.1}}
+         className='bg-gray-200 px-12 py-8 rounded-2xl h-full'>
           <p className='font-extrabold text-black text-lg sm:text-xl'>Premium Plan</p>
           <p className='text-gray-700 text-sm sm:text-base'>Full access to all features</p>
           <p className='mt-6 text-2xl font-bold'>$29.99/month</p>
-        </div>
+        </motion.div>
         
       </div>
       </div>
